3f5cce6f3bf46b06849af2b78be3776bc509c33c
helinu-apple <helinmariaunt@gmail.com>
2026-01-16 21:58:03 +0200

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/alfaarhitektid-reimaged/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
