37558df0c0e99487f2c6f60b81975cc562176a5e
helinu-apple <helinmariaunt@gmail.com>
2026-02-01 18:52:59 +0200

    Replace 'a' tag with 'Link' component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -160,7 +160,7 @@ const Home = () => {
                 <div className="absolute top-0 right-0 w-8 h-8 border-r-2 border-t-2 border-foreground/50"></div>
                 <div className="absolute bottom-0 left-0 w-8 h-8 border-l-2 border-b-2 border-foreground/50"></div>
                 <div className="absolute bottom-0 right-0 w-8 h-8 border-r-2 border-b-2 border-foreground/50"></div>
-              </a>
+              </Link>
             </section>
           </div>
         </div>
